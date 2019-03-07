dacefdc602516c186991aa3567a871aabc534059
Cheter01 <lorenzo.chesi@live.it>
2019-03-07 16:02:19 +0100

    ok

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ var anime = [
 {nome:"The Rising of the Shield Hero", data_inizio:"2019-01-09 17:30:00", episodi:25, bkgImage:"url('images/anime_bk/The-Rising-of-the-Shield-Hero.jpg')"},
 {nome:"Dororo", data_inizio:"2019-01-07 18:00:00", episodi:999999, bkgImage:"url('images/anime_bk/Dororo.jpg')"}, //TODO: numero episodi ignoto
 {nome:"The Promised Neverland", data_inizio:"2019-01-10 20:00:00", episodi:12, bkgImage:"url('images/anime_bk/The-Promised-Neverland.png')"},
-{nome:"Kakegurui XX", data_inizio:"2019-01-08 18:00:00", episodi:999999, bkgImage:"url('images/anime_bk/Kakegurui-XX.png')"} //TODO: numero episodi ignoto
+{nome:"Kakegurui XX", data_inizio:"2019-01-08 18:00:00", episodi:999999, bkgImage:"url('images/anime_bk/Kakegurui-XX.png')"}, //TODO: numero episodi ignoto
+{nome:"Toaru Majutsu no Index III", data_inizio:"2018-10-05 17:00:00", episodi:26, bkgImage:"url('images/anime_bk/Toaru-Majutsu-no-Index-III.jpg')"},
+{nome:"Date A Live 3", data_inizio:"2019-01-13 18:00:00", episodi:26, bkgImage:"url('images/anime_bk/Date-A-Live-3.jpg')"},
+{nome:"JoJo no Kimyou na Bouken: Ougon no Kaze", data_inizio:"2018-10-05 18:00:00", episodi:39, bkgImage:"url('images/anime_bk/JoJo.jpg')"}
 ];
 
 $(window).load(function(){
@@ -84,6 +87,7 @@ $('#countdown').countdown({
 function get_data(){
 	var bk = 0;
 	var now = new Date();
+	//now.setDate(now.getDate() + 4)
 	var data_target = new Date();
 	data_target.setDate(data_target.getDate() + 7);
 	var data_anime = new Date();
@@ -105,7 +109,7 @@ function get_data(){
 	}
 	
 	write_data(bk);
-	console.log(data_target);
+	console.log(now, ", ", data_target);
 	return data_target;
 }
 
